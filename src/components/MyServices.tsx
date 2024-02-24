32b9ea0a8970fb6633bcf8188f9b2ae61219031f
pkellner <peter@peterkellner.net>
2024-02-24 06:26:37 -0800

    go

--- src/components/MyServices.tsx
+++ src/components/MyServices.tsx
@@ -5,6 +5,9 @@ export default function MyServices() {
 
   const toggleDetails = () => setShowDetails(!showDetails);
 
+  // const x = process.env.PUBLIC_KEYSTATIC_GITHUB_APP_SLUG;
+  // console.log("PUBLIC_KEYSTATIC_GITHUB_APP_SLUG: ", x);
+
   return (
     <div className="mx-auto max-w-4xl p-4">
       <h2 className="mb-4 text-center text-2xl font-bold">
@@ -31,7 +34,7 @@ export default function MyServices() {
             data-astro-source-file="/Users/peterkellner/repos/pkellner-blog-astro/pkellner-blog-astropaper/src/components/Socials.astro"
             data-astro-source-loc="22:31"
           >
-            Send an email to Peter Kellner's Blog
+            Send an email to Peter Kellner's Blog   PUBLIC_KEYSTATIC_GITHUB_APP_SLUG:
           </span>{" "}
         </a>
       </h2>
