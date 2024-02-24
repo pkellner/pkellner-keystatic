53f5d689e4dbb4219bdf705fca9c0fef34f70359
pkellner <peter@peterkellner.net>
2024-02-24 07:46:48 -0800

    go

--- astro.config.ts
+++ astro.config.ts
@@ -5,15 +5,16 @@ import remarkToc from "remark-toc";
 import remarkCollapse from "remark-collapse";
 import sitemap from "@astrojs/sitemap";
 import { SITE } from "./src/config";
-
 import alpinejs from "@astrojs/alpinejs";
+import markdoc from "@astrojs/markdoc";
 
+// https://astro.build/config
 export default defineConfig({
   site: SITE.website,
   base: SITE.base,
   integrations: [tailwind({
     applyBaseStyles: false
-  }), react(), sitemap(), alpinejs()],
+  }), react(), sitemap(), alpinejs(), markdoc()],
   markdown: {
     remarkPlugins: [remarkToc, [remarkCollapse, {
       test: "Table of contents"
